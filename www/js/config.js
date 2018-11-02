3b89120714b9fe56a6e9ce0f3046b4aaced1f9c2
fredd2690 <fasdtheunique_26@hotmail.com>
2018-11-01 20:57:55 -0500

    add all files

---
+++ www/js/config.js
@@ -0,0 +1,7 @@
+var krms_config ={			
+	'ApiUrl':"http://restofull.tk/mobileapp/api",		
+	'DialogDefaultTitle':"CuidadEnvios",	
+	'pushNotificationSenderid':"432245605987",	
+	'facebookAppId':"",
+	'APIHasKey':"269f3830d5725527784eec47ddd7x48a"
+	};
